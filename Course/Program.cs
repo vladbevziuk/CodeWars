626dd1446009429a3b1ab6f2990a55144f2dd4ef
vladbevziuk <vladbevziuk71@gmail.com>
2024-02-17 17:26:49 +0100

    Add project files.

---
+++ Course/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            god god = new god();
+            god.poxyi(new[] { 20, 1, -1, 2, -2, 3, 3, 5, 1, 1, 2, 4, 20, 4, -1, -2, 5 });
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Course/god.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Course
+{
+    class god
+    {
+        public void poxyi(int[] seq)
+        {
+            var counts = new Dictionary<int, int>();
+            foreach(int num in seq)
+            {
+                if (counts.ContainsKey(num))
+                {
+                    counts[num]++;
+                }
+                else
+                {
+                    counts[num] = 1;
+                }
+            }
+            foreach(var pair in counts)
+            {
+                if (pair.Value % 2 != 0)
+                {
+                    Console.WriteLine(pair.Key); 
+                }
+            }
+        }
+
+    }
+}
