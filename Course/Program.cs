efb1a18eb05ff0b6bda3911847d2433c5e7c1488
vladbevziuk <vladbevziuk71@gmail.com>
2024-02-20 22:15:20 +0100

    i add new function: functione give me int array, and i'm write this number like telefon number

--- Course/Program.cs
+++ Course/Program.cs
@@ -5,8 +5,8 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            god god = new god(); 
-            god.Likes(new string[0]);
+            god god = new god();
+            god.CreatePhoneNumber(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 });
             Console.ReadLine();
 
         }
--- Course/god.cs
+++ Course/god.cs
@@ -71,6 +71,11 @@ namespace Course
                     return $"{name[0]}, {name[1]} and {name.Length -2} others like this";
             }
         }
+        public static string CreatePhoneNumber(int[] numbers)
+        {
+            return $"({numbers[0]}{numbers[1]}{numbers[2]}) {numbers[3]}{numbers[4]}{numbers[5]}-{numbers[6]}{numbers[7]}{numbers[8]}{numbers[9]}";
+
+        }
 
     }
 }
